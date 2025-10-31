08e211142fe6b6bebda18a5bc2da4868672171c1
Richard Hedlund <richard.hedlund@gmail.com>
2025-10-31 14:53:56 +0100

    led: add initial led module for the bit-bang rgb led driver

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,6 +12,7 @@ use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use {esp_backtrace as _, esp_println as _};
 
+use airmon::led;
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -22,6 +23,8 @@ fn main() -> ! {
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
+
+    led::test();
 
     loop {
         info!("Hello world!");
