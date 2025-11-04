433b8ad5a8536e963216f17639d63327b0bf1304
Richard Hedlund <richard.hedlund@gmail.com>
2025-11-04 01:45:00 +0100

    wip: just a save state of the implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,9 @@
 
 use defmt::info;
 use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
 use esp_hal::main;
+use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::time::{Duration, Instant};
 use {esp_backtrace as _, esp_println as _};
 
@@ -22,14 +24,17 @@ fn main() -> ! {
     // generator version: 0.6.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    let mut gpio8 = Output::new(peripherals.GPIO8, Level::High, OutputConfig::default());
+    let mut delay = Delay::new();
 
-    led::test();
+    let mut rgb_led = led::Led::new(&mut gpio8, &mut delay);
 
     loop {
         info!("Hello world!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
+        rgb_led.set_color(255, 255, 255);
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.1/examples/src/bin
--- src/led.rs
+++ src/led.rs
@@ -1,3 +1,4 @@
+    //set gpio high
 //      Bit bang driver for the WS2812B
 //      Bits encoded as follows:
 //
@@ -20,7 +21,65 @@
 //         |    0.8us    | 0.4us |
 
 use defmt::info;
+use esp_hal::{
+    delay::Delay,
+    gpio::Output,
+    //peripherals::{GPIO, GPIO8},
+    //time::Duration,
+};
+
+//use esp_hal::delay::Delay;
+//use esp_hal::gpio::Output;
+
+pub struct Led<'a> {
+    pin: &'a Output<'a>,
+    delay: &'a Delay,
+    color: u32,
+}
+
+pub fn bit_bang_high(delay: Delay, &mut pin: Output) {
+    delay.delay_nanos(800 as u32);
+    pin.set_high();
+    //set gpio low
+    info!("high");
+}
+
+pub fn bit_bang_low(delay: Delay, pin: Output) {
+    delay.delay_nanos(100 as u32);
+    info!("high");
+}
+
+impl<'a> Led<'a> {
+
+    pub fn new(pin: &'a Output<'a>, delay: &'a Delay) -> Self {
+        Self { pin: pin,
+               delay: delay,
+                color: 0 }
+        }
+
+
+    pub fn set_color(&mut self, red: u8, green: u8, blue: u8) {
+
+        self.color |= (red as u32) << 16;
+        self.color |= (green as u32) << 8;
+        self.color |= blue as u32;
+
+        info!("RGB LED: changing color");
+        for i in 0..32 {
+            if (self.color >> i) & 1 == 1
+            {
+                bit_bang_high(*self.delay, self.pin);
+            }
+            else {
+                bit_bang_low(*self.delay, self.pin);
+            }
+        }
+        info!("RGB LED: color change complete");
+    }
+
+}
 
 pub fn test() {
     info!("LED test");
 }
+
